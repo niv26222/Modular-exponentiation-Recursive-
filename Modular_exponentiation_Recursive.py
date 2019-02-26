d81b3e88b21ff867401e8df4ab63f09a2fc2a20b
Niv Ben Abat <niv.benavat@gmail.com>
2019-02-26 18:39:19 +0200

    Add files via upload

---
+++ Modular_exponentiation_Recursive.py
@@ -0,0 +1,20 @@
+def exponentMod(A, B, C): 
+      
+    # Base Cases 
+    if (A == 0): 
+        return 0
+    if (B == 0): 
+        return 1
+      
+    # If B is Even 
+    y = 0
+    if (B % 2 == 0): 
+        y = exponentMod(A, B / 2, C) 
+        y = (y * y) % C 
+      
+    # If B is Odd 
+    else: 
+        y = A % C 
+        y = (y * exponentMod(A, B - 1,  
+                             C) % C) % C 
+    return ((y + C) % C) 
